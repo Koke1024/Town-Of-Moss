68419d4d172ad9c6cccbdcd074398c6b5e73de56
Koke <koke1024@gmail.com>
2021-10-26 03:24:52 +0900

    JanitorのCleanクールダウンを会議後にリセット

---
+++ Patches/Roles/ImpostorRoles/JanitorMod/HUDClose.cs
@@ -0,0 +1,24 @@
+using System;
+using HarmonyLib;
+using TownOfUs.Extensions;
+using TownOfUs.Roles;
+using DateTime = Il2CppSystem.DateTime;
+using Object = UnityEngine.Object;
+
+namespace TownOfUs.ImpostorRoles.JanitorMod
+{
+    [HarmonyPatch(typeof(Object), nameof(Object.Destroy), typeof(Object))]
+    public static class HUDClose
+    {
+        public static void Postfix(Object obj)
+        {
+            if (ExileController.Instance == null || obj != ExileController.Instance.gameObject) return;
+            if (!PlayerControl.LocalPlayer.Is(RoleEnum.Janitor)) {
+                return;
+            }
+
+            Janitor janitor = Role.GetRole<Janitor>(PlayerControl.LocalPlayer);
+            janitor.lastCleaned = DateTime.UtcNow;
+        }
+    }
+}
