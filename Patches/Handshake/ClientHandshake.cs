a81c0529ea1724da30a092be4cd9d51d99673b70
Koke <koke1024@gmail.com>
2021-10-28 05:27:31 +0900

    version

--- Patches/Handshake/ClientHandshake.cs
+++ Patches/Handshake/ClientHandshake.cs
@@ -14,7 +14,7 @@ namespace TownOfUs.Handshake
     {
         private const byte TOU_ROOT_HANDSHAKE_TAG = 69;
         
-        private const int MOSS_VERSION = 37;
+        private const int MOSS_VERSION = 378　;
 
         [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.OnGameJoined))]
         public static class AmongUsClient_OnGameJoined
