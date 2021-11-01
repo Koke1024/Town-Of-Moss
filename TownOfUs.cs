99411bcdd838e74091e5f0d0ccf27a25b0d1c8de
Koke <koke1024@gmail.com>
2021-11-01 11:18:50 +0900

    0.393

--- TownOfUs.cs
+++ TownOfUs.cs
@@ -20,11 +20,11 @@ using UnityEngine.SceneManagement;
 
 namespace TownOfUs
 {
-    [BepInPlugin(Id, "Town Of Moss", "0.392")]
+    [BepInPlugin(Id, "Town Of Moss", "0.393")]
     [BepInDependency(ReactorPlugin.Id)]
     public class TownOfUs : BasePlugin
     {
-        public static string Version = "0.392";
+        public static string Version = "0.393";
         public const string Id = "jp.spiel.koke";
 
         public static Sprite JanitorClean;
