df8a1377f6a8559cbd44186518a5c5d16e66466c
Koke <koke1024@gmail.com>
2021-11-02 22:57:43 +0900

    バージョン部分のみ書き換え

--- TownOfUs.cs
+++ TownOfUs.cs
@@ -20,11 +20,11 @@ using UnityEngine.SceneManagement;
 
 namespace TownOfUs
 {
-    [BepInPlugin(Id, "Town Of Moss", "0.394")]
+    [BepInPlugin(Id, "Town Of Moss", "0.395")]
     [BepInDependency(ReactorPlugin.Id)]
     public class TownOfUs : BasePlugin
     {
-        public static string Version = "0.394";
+        public static string Version = "0.395";
         public const string Id = "jp.spiel.koke";
 
         public static Sprite JanitorClean;
